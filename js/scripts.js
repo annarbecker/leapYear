8f18f240779ce027540e134d45d595a981b17e4b
Anna Becker and Tyler Suderman <tcsuder@gmail.com>
2016-01-19 09:57:09 -0800

    Add functionality for NaN and refactor code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
+  return ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0) && (year * 0 === 0)) ;
 };
+
+
+
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    if (year * 0 !== 0) {
+      $("#notNumber").show();
+    } else {
+      $(".year").text(year);
+
+      if (!result) {                 // same as writing if (result === false)
+        $(".not").text("not");
+      }
+      else {
+        $(".not").text("");
+      }
+      $("#result").show();
+      $("#notNumber").hide();
+    }
+
+    event.preventDefault();
+  });
+});
